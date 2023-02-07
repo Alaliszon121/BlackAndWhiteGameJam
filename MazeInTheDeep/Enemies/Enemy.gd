e68bff7a9fe73f4f72f93b7cf11be456ba6bb897
Alaliszon121 <isabelle.prince.umh@gmail.com>
2023-02-07 17:08:47 +0100

    enemy movement iteration

--- MazeInTheDeep/Enemies/Enemy.gd
+++ MazeInTheDeep/Enemies/Enemy.gd
@@ -1,12 +1,16 @@
 extends KinematicBody2D
 
 export var path_to_player := NodePath()
+export var path_of_positions := NodePath()
 
 var _velocity := Vector2.ZERO
-
+var can_see_player := false
+var counter = 2
+ 
 onready var _agent: NavigationAgent2D = $NavigationAgent2D
 onready var _timer: Timer = $Timer
 onready var _player := get_node(path_to_player)
+onready var _positions := get_node(path_of_positions)
 
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
@@ -18,6 +22,9 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	if _agent.is_navigation_finished():
+		counter += 1
+		if counter > 3:
+			counter = 0
 		return
 	
 	var target_global_position := _agent.get_next_location()
@@ -29,9 +36,14 @@ func _physics_process(delta: float) -> void:
 	_velocity += steering
 	
 	_velocity = move_and_slide(_velocity)
-	
+	$raycast.rotation = _velocity.angle()
 	animationTree.set("parameters/Walk/blend_position", _velocity.normalized())
 	animationState.travel("Walk")
 
 func _update_pathfinding() -> void:
-	_agent.set_target_location(_player.global_position)
+	#print("update!")
+	if !can_see_player:
+		_agent.set_target_location(_positions.get_child(counter).position)
+		
+	else:
+		_agent.set_target_location(_player.global_position)
