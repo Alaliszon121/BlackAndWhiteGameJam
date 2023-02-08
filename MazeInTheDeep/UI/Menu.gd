bb10522a14172a9caec2fbb7b822b7e032f85b96
Arvi-beep-boop <xarvichanx@gmail.com>
2023-02-08 23:27:51 +0100

    updated menu

---
+++ MazeInTheDeep/UI/Menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/StartButton.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://Levels/Test Level.tscn")
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
